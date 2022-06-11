7a571193449cc326f8da89004723671d1cc99d9d
jottadev <contato.joaoythalo@gmail.com>
2022-06-11 18:53:13 -0300

    Finalização do exercício

---
+++ script.js
@@ -0,0 +1,20 @@
+
+let age = 16
+if(age => 18){
+    console.log('A idade é menor que 18 anos.')
+}
+
+let myage = 19
+if(myage => 18){
+    console.log(true)
+}
+
+let month1 = 'Janeiro'
+let month2 = 'Dezembro'
+let birthday = 'Aniversário'
+if(birthday == month1){
+    console.log(`Você faz aniversário em ${month1}.`)
+} else{
+    console(`Você faz aniversário em ${month2}.`)
+}
+
